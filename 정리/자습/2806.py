38e126e53f1d3e9072c228632b77a6112ef134ee
ssafy-kun <alqp118@gmail.com>
2022-03-31 19:35:09 +0900

    add swea

--- 정리/자습/2806.py
+++ 정리/자습/2806.py
@@ -2,33 +2,69 @@ import sys
 sys.stdin = open("input.txt", "r")
 
 
-# 체스놓을 수 있는 경우의 수
-dx = [1, 1, 1]
-dy = [-1, 0, 1]
-
-
-
 def DFS(sx, sy, s):
+    global cnt
+
     if s == N:
+        cnt += 1
         return
-    for k in range(3):
-        nx = sx +dx[k]
-        ny = sy +dy[k]
-        # arr[nx][ny]기준 체스판 전체 팔방으로 퀸이 없으면 놓을 수 있음
-        for i in range(N):
-            # 상하좌우와 대각선
-            if arr[nx][i] or arr[i][nx]:
 
+    if sx < 0 or sx >= N or sy < 0 or sy >= N:
+        return
 
+    # 체스판 전체 범위 팔방에 퀸이 없으면 퀸놓기
+    put = 1
+    # 상 체크
+    for x in range(sx):
+        if arr[x][sy]:
+            put = 0
+            break
+    # 왼쪽 체크
+    for y in range(sy):
+        if arr[sx][y]:
+            put = 0
+            break
 
+    # 대각선 체크
+    # 좌상 체크
+    nx = sx - 1
+    ny = sy - 1
+    while 0 <= nx < N and 0 <= ny < N:
+        if arr[nx][ny]:
+            put = 0
+            break
+        nx -= 1
+        ny -= 1
+    # 우상 체크
+    nx = sx - 1
+    ny = sy + 1
+    while 0 <= nx < N and 0 <= ny < N:
+        if arr[nx][ny]:
+            put = 0
+            break
+        nx -= 1
+        ny += 1
+    # 다체크 했는데 put이 1이면 말을 놓고 DFS
 
+    # 말을 놓을 수 있다면
+    if put == 1:
+        arr[sx][sy] = 1
+        DFS(sx + 1, 0, s + 1)
+        # 다 돌아 봤는데 아니었으면 말치우고
+        arr[sx][sy] = 0
 
-
+    # 옆칸 확인하러가기
+    DFS(sx, sy + 1, s)
 
 
 T = int(input())
-for tc in range(1, T+1):
+for tc in range(1, T + 1):
     N = int(input())
     # 체스판
-    arr = [[0]*N for _ in range(N)]
-    for
+    arr = [[0] * N for _ in range(N)]
+    cnt = 0
+
+    for i in range(N):
+        DFS(i, 0, 0)
+
+    print(f'#{tc} {cnt}')
